855ad4bc26e86f3d45c831732627027f9c81877e
elysia-lys <176448029+elysia-lys@users.noreply.github.com>
2026-07-31 15:32:09 +0800

    ok

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: yeliew <yeliew@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/31 14:32:49 by yeliew            #+#    #+#             */
-/*   Updated: 2026/07/31 14:35:09 by yeliew           ###   ########.fr       */
+/*   Updated: 2026/07/31 15:31:56 by yeliew           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char	*array;
+	size_t	i;
 
+	if (!s || !f)
+		return (0);
+	array = malloc(ft_strlen(s) + 1);
+	if (!array)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		array = f(i, s[i]);
+		i++;
+	}
+	array[i] = 0;
+	return (array);
 }
